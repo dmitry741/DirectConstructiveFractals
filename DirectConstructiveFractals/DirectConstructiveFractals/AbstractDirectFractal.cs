0f87fe5b7f35399c604803163fb2b4a0ef44e8ea
Dmitriy Pavlov <dmitriy.pavlov@danfoss.com>
2020-07-08 10:12:50 +0300

    The first coomit from VS

---
+++ DirectConstructiveFractals/DirectConstructiveFractals/AbstractDirectFractal.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace DirectConstructiveFractals
+{
+    abstract class AbstractDirectFractal : IConstructiveFractal
+    {
+        protected double[] _angles = null;
+        protected float _lenght = 0;
+        protected int _fragmentsPerStep = 0;
+
+        private IEnumerable<int> ConvertNumericSystem(int v)
+        {
+            if (v == 0)
+                return new List<int>() { 0 };
+
+            List<int> list = new List<int>();
+            int b = v;
+
+            while (b > 0)
+            {
+                list.Add(b % _fragmentsPerStep);
+                b /= _fragmentsPerStep;
+            }
+
+            return list;
+        }
+
+        private double GetAngle(int step)
+        {
+            IEnumerable<int> numbers = ConvertNumericSystem(step);
+            return numbers.Select(n => _angles[n]).Sum();
+        }
+
+        private PointF Rotate(PointF point, PointF center, double angle)
+        {
+            double co = Math.Cos(angle);
+            double si = Math.Sin(angle);
+
+            double x = co * (point.X - center.X) + si * (point.Y - center.Y) + center.X;
+            double y = -si * (point.X - center.X) + co * (point.Y - center.Y) + center.Y;
+
+            return new PointF { X = Convert.ToSingle(x), Y = Convert.ToSingle(y) };
+        }
+
+        public IEnumerable<PointF> Build(int N, PointF startPoint, PointF endPoint)
+        {
+            int numberOfSegments = 1;
+            float len = 1.0f;
+
+            for (int i = 0; i < N; i++)
+            {
+                numberOfSegments *= _fragmentsPerStep;
+                len *= _lenght;
+            }
+
+            List<PointF> points = new List<PointF> { startPoint };
+            PointF vector = new PointF(endPoint.X - startPoint.X, endPoint.Y - startPoint.Y);
+
+            for (int i = 0; i < numberOfSegments; i++)
+            {
+                PointF preparePoint = new PointF(points[i].X + vector.X * len, points[i].Y + vector.Y * len);
+                PointF p = Rotate(preparePoint, points[i], GetAngle(i));
+                points.Add(p);
+            }
+
+            return points;
+        }
+    }
+}
---
+++ DirectConstructiveFractals/DirectConstructiveFractals/IConstructiveFractal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace DirectConstructiveFractals
+{
+    /// <summary>
+    /// Декларация базовых методов конструктивных фракталолв.
+    /// </summary>
+    interface IConstructiveFractal
+    {
+        /// <summary>
+        /// Построение конструктивного фрактала.
+        /// </summary>
+        /// <param name="N">Число шагов.</param>
+        /// <param name="startPoint">Начальная точка сегмента.</param>
+        /// <param name="endPoint">Конечная точка сегмента.</param>
+        /// <returns>Коллекция точек PointF.</returns>
+        IEnumerable<PointF> Build(int N, PointF startPoint, PointF endPoint);
+    }
+}
